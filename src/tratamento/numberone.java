bc0b2546c7902c0b1906eddc6f655a5826e1e2be
Aluno IFMT-PDL <aluno@ifmt.edu>
2023-09-05 18:12:30 -0400

    commit

---
+++ src/tratamento/numberone.java
@@ -0,0 +1,20 @@
+package tratamento;
+
+public class numberone {
+
+	public static void main(String[] args) {
+		try {
+			int numerator = 10;
+			int denominator 0;
+			int result = numerator (denominator);
+		}catch (ArithmeticException) {
+			System.out.println("houve um erro aritmetico");
+		}
+		System.out.println("resultado:"+result);
+	
+	
+
+	}
+
+}
+
---
+++ src/tratamento/numbertwo.java
@@ -0,0 +1,18 @@
+package tratamento;
+
+public class numbertwo {
+
+	public static void main(String[] args) {
+		int number;
+		try{
+			String input: ("abc");
+			int number1 = integer.parseint(input);
+		}catch (NumberFormatException e) {
+			System.out.println("ocorreu um erro ao tentar converter");
+		
+			
+		}
+System.out.println("Number:" +number );
+	}
+
+}
